29ae67cc32da0e5e4fdd3649cb61c3674de403f5
andrewleachtx <andrewleach38@gmail.com>
2025-08-05 17:02:10 -0500

    Add constructors and copy assignment

--- include/Vector.h
+++ include/Vector.h
@@ -15,6 +15,8 @@ public:
     using const_pointer = std::allocator_traits<Allocator>::const_pointer;
     using iterator = Iterator;
     using const_iterator = const Iterator;
+
+    using traits = std::allocator_traits<allocator_type>;
     
     struct Iterator {
         public:
@@ -50,17 +52,87 @@ public:
             pointer m_ptr = nullptr;
     };
 
-    Vector() { /* TODO */ }
-    explicit Vector(size_type count, const allocator_type &alloc = allocator_type()) { /* TODO */ }
-    constexpr Vector(size_type count, const_reference value = value_type(), const allocator_type &alloc = allocator_type()) { /* TODO */ }
-    constexpr Vector(const Vector &other) { /* TODO */ }
-    constexpr Vector(Vector &&other) noexcept { /* TODO */ }
-    constexpr Vector& operator=(const Vector &other) { /* TODO */ }
+    Vector() = default;
+    explicit Vector(size_type count, const allocator_type &alloc = allocator_type()) : m_data(nullptr) , m_size(count), m_capacity(count), m_alloc(alloc) {
+        m_data = traits::allocate(alloc, count);
 
-    Vector& operator=(Vector &&other) noexcept { /* TODO */ }
+        for (size_type i = 0; i < count; i++) {
+            std::allocator<allocator_type>::construct(m_alloc, m_data + i, value_type());
+        }
+    }
+    constexpr Vector(size_type count, const_reference value = value_type(), const allocator_type &alloc = allocator_type()) {
+        m_data = traits::allocate(m_alloc, count);
+
+        for (size_type i = 0; i < count; i++) {
+            std::allocator<allocator_type>::construct(m_alloc, m_data + i, value);
+        }
+    }
+    constexpr Vector(const Vector &other)
+        : m_data(nullptr),
+        m_size(other.m_size),
+        m_capacity(other.m_capacity),
+        m_alloc(std::allocator_traits::select_on_container_copy_construction(other.m_alloc)) {
+            
+        m_data = traits::allocate(m_alloc, m_capacity);
+        
+        for (size_type i = 0; i < m_size; i++) {
+            traits::construct(m_alloc, m_data + i, other.m_data[i]);
+        }
+    }
+    constexpr Vector(Vector &&other) noexcept
+        : m_data(other.m_data),
+        m_size(other.m_size),
+        m_capacity(other.m_capacity),
+        m_alloc(std::move(other.m_alloc)) {
+
+        other.m_data = nullptr;
+        other.m_size = 0;
+        other.m_capacity = 0;
+    }
+
+    ~Vector() {
+        for (size_type i = 0; i < m_size; i++) {
+            traits::destroy(m_alloc, m_data + i);
+        }
+        
+        if (m_data) {
+            traits::deallocate(m_alloc, m_data, m_capacity);
+        }
+    }
+
+    constexpr Vector& operator=(const Vector &other) {
+        if (this != &other) {
+            // Assume we need to destroy all of our values even if we don't
+            for (size_type i = 0; i < m_size; i++) {
+                traits::destroy(m_alloc, m_data + i);
+            }
+            
+            // TODO: Possibly check for difference in capacity to avoid unnecessary alloc/dealloc
+            traits::deallocate(m_alloc, m_data, m_capacity);
+
+            if constexpr (traits::propagate_on_container_copy_assignment::value) {
+
+            }
+            m_alloc = traits::select_on_container_copy_construction(other.m_alloc);
+            m_size = other.m_size;
+            m_capacity = other.m_capacity;
+            
+            m_data = traits::allocate(m_alloc, m_capacity);            
+            for (size_type i = 0; i < m_size; i++) {
+                traits::construct(m_alloc, m_data + i, other.m_data[i]);
+            }
+        }
+
+        return *this;
+    }
+    Vector& operator=(Vector &&other) noexcept {
+        if (this != &other) {
+            
+        }
+        
+        return *this;
+    }
     Vector& operator=(std::initializer_list<value_type> ilist) { /* TODO */ }
-    
-    constexpr ~Vector() { /* TODO */ }
 
     constexpr void assign(size_type count, const_reference value) { /* TODO */ }
     constexpr void assign(std::initializer_list<value_type> ilist) { /* TODO */ }
@@ -91,8 +163,8 @@ public:
     constexpr const_iterator cend() const noexcept { /* TODO */ }
 
     private:
-        pointer m_arr;
-        [[no_unique_address]] allocator_type m_alloc;
+        pointer m_data;
         size_type m_size;
         size_type m_capacity;
+        [[no_unique_address]] allocator_type m_alloc;
 };
